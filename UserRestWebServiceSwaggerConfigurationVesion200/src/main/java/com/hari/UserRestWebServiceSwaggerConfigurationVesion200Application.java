b01550aa69f0da0ca74f5c13d3318727560c4d4e
Narahari <Narahari@192.168.0.6>
2019-12-30 21:59:55 +0530

    Swagger Example With Spring boot 2.0.0

---
+++ UserRestWebServiceSwaggerConfigurationVesion200/src/main/java/com/hari/UserRestWebServiceSwaggerConfigurationVesion200Application.java
@@ -0,0 +1,13 @@
+package com.hari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRestWebServiceSwaggerConfigurationVesion200Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRestWebServiceSwaggerConfigurationVesion200Application.class, args);
+	}
+
+}
---
+++ UserRestWebServiceSwaggerConfigurationVesion200/src/test/java/com/hari/UserRestWebServiceSwaggerConfigurationVesion200ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRestWebServiceSwaggerConfigurationVesion200ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
